18b7bf73b0e72fd31d0a12164f5dca794f586c21
Prabhash Rai <raiprabh@groww.in>
2023-01-23 14:56:59 +0530

    checked winner

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -7,11 +7,16 @@ function Board() {
         ['', '', ''],
         ['', '', '']
     ]);
+    const [winner, setWinner] = useState('');
 
     let [moves, setMoves] = useState(1);
 
     function cellClicked(rowNo, colNo) {
+        if (winner !== '') return;
+
         let oldBoard = [...currentBoard];
+
+        if (oldBoard[rowNo][colNo] !== '') return;
 
         if (moves % 2 === 1) {
             oldBoard[rowNo][colNo] = 'X';
@@ -23,8 +28,40 @@ function Board() {
         }
 
         setCurrentBoard(oldBoard);
-        console.log(moves)
+
+        if (moves >= 4) {
+            checkWinner();
+        }
     }
+
+    function checkWinner() {
+        if (winner !== '') return;
+
+        for (let i = 0; i<3; i++) {
+            // check rows
+            if (currentBoard[i][0] !== '') {
+                if (currentBoard[i][0] === currentBoard[i][1] && currentBoard[i][0] === currentBoard[i][2]) {
+                    setWinner(currentBoard[i][0]);
+                }
+            }
+            // check columns
+            if (currentBoard[0][i] !== '') {
+                if (currentBoard[0][i] === currentBoard[1][i] && currentBoard[0][i] === currentBoard[2][i]){
+                    setWinner(currentBoard[0][i]);
+                }
+            }
+        }
+
+        //check diagonals
+        if (currentBoard[0][0] !== '') {
+            if(currentBoard[0][0] === currentBoard[1][1] && currentBoard[0][0] === currentBoard[2][2]) setWinner(currentBoard[0][0]);
+        }
+
+        if (currentBoard[0][2] !== '') {
+            if(currentBoard[0][2] === currentBoard[1][1] && currentBoard[0][2] === currentBoard[2][0]) setWinner(currentBoard[0][2]);
+        }
+    }
+
 
     return (
         <div className='Board'>
