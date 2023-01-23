75c222f003b7448fa3041e69a495d7986446be72
Prabhash Rai <raiprabh@groww.in>
2023-01-23 13:25:12 +0530

    added click function

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -8,24 +8,46 @@ function Board() {
         ['', '', '']
     ]);
 
+    const [player, setPlayer] = useState(1);
+
+    function cellClicked(rowNo, colNo) {
+        console.log(rowNo, colNo)
+        let oldBoard = [...currentBoard];
+
+        if (player === 1) {
+            oldBoard[rowNo][colNo] = 'X';
+            setPlayer(2);
+        }
+        else {
+            oldBoard[rowNo][colNo] = 'O';
+            setPlayer(1);
+        }
+
+        setCurrentBoard(oldBoard);
+    }
+
     return (
         <div className='Board'>
-            {
-                currentBoard.map(
-                    function (row, rowNo) {
-                        return <div className='board_row'>
-                            {
-                                row.map(
-                                    function(cell, index) {
-                                        return <div className='board_cell'>({rowNo}, {index})</div>;
+            <table>
+                <tbody>
+                    {
+                        currentBoard.map(
+                            function (rowElement, rowNo) {
+                                return <tr id='{rowNo}' className='board_row'>
+                                    {
+                                        rowElement.map(
+                                            function (cellElement, colNo) {
+                                                return <td id='{colNo}' className='board_cell' onClick={() => cellClicked(rowNo, colNo)}>{cellElement}</td>;
+                                            }
+                                        )
                                     }
-                                )
+                                </tr>
+
                             }
-                        </div>
-                        
+                        )
                     }
-                )
-            }
+                </tbody>
+            </table>
         </div>
     )
 }
