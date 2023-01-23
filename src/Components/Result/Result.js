f06fea862c133bf8dd2eb20c74d25b5b5e9de399
Prabhash Rai <raiprabh@groww.in>
2023-01-24 00:18:22 +0530

    fixed css

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -5,7 +5,7 @@ function Result({ winner, moves }) {
     return (
         <div className='Result'>
             {
-                winner === '' && moves === 9 ? <p>It's a tie</p> : <p>{winner} won the game.</p>
+                winner === '' && moves === 9 ? <h1>It's a tie</h1> : <h1>{winner} won the game.</h1>
             }
         </div>
     )
